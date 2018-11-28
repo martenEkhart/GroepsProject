a1b82fc7a4ad4d487abefe79d9f7c49cd60ff494
willem <willem.roos@live.nl>
2018-11-28 09:51:43 +0100

    Deleted csss link

--- resources/views/opmaak/app.blade.php
+++ resources/views/opmaak/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/WebshopProject/public/css/app.css">
     <link rel="stylesheet"  href="/css/app.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -19,10 +18,6 @@
     </div>        
 
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-    <script>
-        CKEDITOR.replace( 'article-ckeditor' );
-    </script>
 
 </body>
 </html>
