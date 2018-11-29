3cb07672d95a3e332ed71d4c1875e3061d78cd32
martenEkhart <martenekhart@hotmail.com>
2018-11-29 09:31:00 +0100

    uuhm

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -27,8 +27,40 @@
               </div>
             </li>
           </ul>
-          <form class="form-inline my-2 my-md-0">
+          {{-- <form class="form-inline my-2 my-md-0">
             <input class="form-control" type="text" placeholder="Search">
-          </form>
+          </form> --}}
+          <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                <li class="nav-item active">
+                    @if (Route::has('register'))
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    @endif
+                </li>
+            @else
+                <li class="nav-item dropdown">
+                    
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <a  class="dropdown-item" href="/dashboard"></a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
         </div>
       </nav>
