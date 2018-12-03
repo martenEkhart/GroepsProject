22d14f170e668843c6a981b0274fd978c9575508
walter086 <wro1986@live.nl>
2018-12-03 09:09:08 +0000

    (w)Altered a migration

--- database/migrations/2018_12_03_090814_create_payments_table.php
+++ database/migrations/2018_12_03_090814_create_payments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PurchasesTable extends Migration
+class CreatePaymentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class PurchasesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class PurchasesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('payments');
     }
 }
