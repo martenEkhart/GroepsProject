b9b34b21e44d808e7e2966d6ba47ef79d1883ee3
walter086 <wro1986@live.nl>
2018-12-04 13:50:49 +0000

    paymentcontroller changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -65,67 +65,84 @@ public function statusPayment(Request $request)
 }
 
 public function handle(Request $request) {
+    // Handles webhookfeedback from Mollie
     if (! $request->has('id')) {
         echo "test";
         return;
     }
+    
+    $payment_to_db = new Payment();
+    $payment_to_db->mollie_id = $request->id;
+    $payment_to_db->save();
+    // get status from mollie and determine what to do
+    $payment = Mollie::api()->payments()->get($request->id);
+    // switch van maken:
+    //  if ($payment->isPaid())
+    //  {
+    //     $payment_status = Payment::where('mollie_id',$request->id)->first();
+    //     $payment_status->status = '2'; // paid
+    //     $payment_status->save();
+    //  }
 
+    //  else if ($payment->isOpen())
+    //  {
+    //     $payment_status = Payment::where('mollie_id',$request->id)->first();
+    //     $payment_status->status = '0'; // open
+    //     $payment_status->save();
+    //  }
+    //  else if ($payment->isCanceled())
+    //  {
+    //     $payment_status = Payment::where('mollie_id',$request->id)->first();
+    //     $payment_status->status = '1'; // canceled
+    //     $payment_status->save();
+    //  }
+    //  else if ($payment->isPending())
+    //  {
+    //     $payment_status = Payment::where('mollie_id',$request->id)->first();
+    //     $payment_status->status = '3'; // pending
+    //     $payment_status->save();
+    //  }
 
- 
-     $payment_to_db = new Payment();
-     $payment_to_db->mollie_id = $request->id;
-     $payment_to_db->save();
+    //  else if ($payment->isAuthorized())
+    //  {
+    //     $payment_status = Payment::where('mollie_id',$request->id)->first();
+    //     $payment_status->status = '4'; // Authorized
+    //     $payment_status->save();
+    //  }
 
-     $payment = Mollie::api()->payments()->get($request->id);
-     if ($payment->isPaid())
-     {
-        $payment_status = Payment::where('mollie_id',$request->id)->first();
-        $payment_status->status = '2'; // paid
-        $payment_status->save();
-     }
+    //  else if ($payment->isExpired())
+    //  {
+    //     $payment_status = Payment::where('mollie_id',$request->id)->first();
+    //     $payment_status->status = '5'; // Expired
+    //     $payment_status->save();
+    //  }
 
-     else if ($payment->isOpen())
-     {
-        $payment_status = Payment::where('mollie_id',$request->id)->first();
-        $payment_status->status = '0'; // open
-        $payment_status->save();
-     }
-     else if ($payment->isCanceled())
-     {
-        $payment_status = Payment::where('mollie_id',$request->id)->first();
-        $payment_status->status = '1'; // canceled
-        $payment_status->save();
-     }
-     else if ($payment->isPending())
-     {
-        $payment_status = Payment::where('mollie_id',$request->id)->first();
-        $payment_status->status = '3'; // pending
-        $payment_status->save();
-     }
+    //  else if ($payment->isFailed())
+    //  {
+    //     $payment_status = Payment::where('mollie_id',$request->id)->first();
+    //     $payment_status->status = '6'; // Failed
+    //     $payment_status->save();
+    //  }
 
-     else if ($payment->isAuthorized())
-     {
-        $payment_status = Payment::where('mollie_id',$request->id)->first();
-        $payment_status->status = '4'; // canceled
-        $payment_status->save();
-     }
-
-     else if ($payment->isExpired())
-     {
-        $payment_status = Payment::where('mollie_id',$request->id)->first();
-        $payment_status->status = '5'; // canceled
-        $payment_status->save();
-     }
-
-     else if ($payment->isFailed())
-     {
-        $payment_status = Payment::where('mollie_id',$request->id)->first();
-        $payment_status->status = '6'; // canceled
-        $payment_status->save();
-     }
-
+    //  else {
+    //      // error, geen status terug gekregen van Moillie. What to do?
+    //  }
 
      
+switch ($payment) {
+    case isPaid():
+    $payment_status = Payment::where('mollie_id',$request->id)->first();
+    $payment_status->status = '2'; // paid
+    $payment_status->save();
+        break;
+    case "bar":
+        echo "i is bar";
+        break;
+    case "cake":
+        echo "i is cake";
+        break;
+}
+
 
 }
 
