26760f65637e46d1ecbf2c48a893de286b84e42c
walter086 <wro1986@live.nl>
2018-12-05 14:05:34 +0000

    Fixing  saving payment method

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -58,7 +58,7 @@ public function handle(Request $request) {
     $payment_to_db->order_id = $order_id;
     $payment_to_db->currency = $currency;
     $payment_to_db->amount = $amount;
-    // $payment_to_db->method = $methods;
+    $payment_to_db->method = $method;
     $payment_to_db->save();
     // Get payment status from mollie and determine what to do
     
