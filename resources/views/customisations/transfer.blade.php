662ff1ffe9dc38ce7eacefefd994f81f4dbc4e67
Raoul Witsenburg <raoulwitsenburg@gmail.com>
2018-12-06 16:27:03 +0100

    transfer.blade aanpassingen

--- resources/views/customisations/transfer.blade.php
+++ resources/views/customisations/transfer.blade.php
@@ -69,29 +69,47 @@
    
 </div>
       
-<img id="img" src="../images/customisations/p2.png" alt="Smiley face" height="auto" width="auto" style="left: 0px; position:absolute">
+{{-- <img id="img" src="../images/customisations/p2.png" alt="" height="auto" width="auto" style="left: 0px; position:absolute"> --}}
 <div id="tixt">
 
 </div>
 
- 
 <script>
+
+function createImg(nr) {
+    c = customisations[nr];
+    cm[nr] = document.createElement('img');
+    cm[nr].style.position = "absolute";
+    cm[nr].src = "../images/customisations/" + c.image_name;
+    cm[nr].style.left = c.x+ dotted.left + "px";
+    cm[nr].style.top = c.y + dotted.top + "px";
+    cm[nr].height = c.height;
+    cm[nr].width = c.width;
+    cm[nr].style.zIndex = c.z_layer;
+    cm[nr].style.opacity = c.opacity/100;
+    document.body.appendChild(cm[nr]);
+    
+}   
+
  var counter = 0;
- var moveImage = true;
- var c=document.getElementById("canvas");
- var dotted = document.getElementById("dott").getBoundingClientRect();//document.getElementById("dotted");
- //var im = document.getElementById("imga");
- var img = document.getElementById("img");
- img.style.opacity = 0.7;
- //im.style.left = dotted.left +"px";
- //im.style.top = dotted.top +"px";
- img.style.left = dotted.left +"px";
- img.style.top = dotted.top +"px";
+ var cm = [];
+
+ var dotted = document.getElementById("dott").getBoundingClientRect();
+//  var img = document.getElementById("img");
+//  img.style.opacity = 0.7;
+//  img.style.left = dotted.left +"px";
+//  img.style.top = dotted.top +"px";
  
 
   var tel = 0;
-  im1 = document.getElementById("img");
+//   im1 = document.getElementById("img");
   tixt = document.getElementById("tixt");
+
+  
+
+    function updateForm() {
+
+    }
   
     function fnTurn(dx) {
         if((counter <= 0)&&(dx == -1)) {
@@ -104,30 +122,30 @@
             }
 
         }
-        tixt.innerHTML = counter;
         fnVolgende(counter);
     }
-    
+
     function fnVolgende(counter) {
-        document.getElementById("name").innerHTML = customisations[counter].name;
-        im1.src = "../images/customisations/" + customisations[counter].image_name;
-        im1.style.left = customisations[counter].x;
-        im1.style.top = customisations[counter].y;
-        im1.height = customisations[counter].height;
-        im1.style.zIndex = customisations[counter].z_layer;
-        im1.style.opacity = customisations[counter].opacity;
+        document.getElementById("name").innerHTML = customisations[counter].name+ " id:"+customisations[counter].id;
+        im1 = cm[counter];
+        // im1.src = "../images/customisations/" + customisations[counter].image_name;
+        // im1.style.left = customisations[counter].x;
+        // im1.style.top = customisations[counter].y;
+        // im1.height = customisations[counter].height;
+        // im1.width = customisations[counter].width;
+        // im1.style.zIndex = customisations[counter].z_layer;
+        // im1.style.opacity = customisations[counter].opacity/100;
         document.getElementById("checkbox").checked = customisations[counter].visible;
     }
 
     function fnMouseZindex(e) {
-        img.style.zIndex = e.clientX-parseInt(document.getElementById("progressZindex").style.width);
+        im1.style.zIndex = e.clientX-parseInt(document.getElementById("progressZindex").style.width);
        tixt.innerHTML = e.clientX-(parseInt(document.getElementById("progressZindex").style.width));
         // alert(e.clientX);
     }
 
     function fnMouseOpacity(e) {
-        img.style.opacity = (e.clientX-50)/(parseInt(document.getElementById("progressOpacity").style.width)*3);
-       tixt.innerHTML = e.clientX;
+        im1.style.opacity = (e.clientX-50)/(parseInt(document.getElementById("progressOpacity").style.width)*3);
        // alert(e.clientX);
     }
 
@@ -146,13 +164,12 @@
   function fnVert (dx) {
     if(((parseInt(im1.style.top)<-70)&&(dx<0))||(((parseInt(im1.style.top)+parseInt(im1.height))>window.innerHeight-50)&&(dx >0))) { return }
     im1.style.top = parseInt(im1.style.top)+dx +"px";  
-   // tixt.innerHTML = parseInt(im1.style.top) + "  "+parseInt(window.innerHeight-100);
-   tixt.innerHTML =im1.style.bottom + "  jkd  " +parseInt(im1.style.top)+parseInt(im1.height);
-  }
+   }
  
   function fnSize(size) {
     if(((parseInt(im1.height)<20)&&(size<1))||((parseInt(im1.style.width)>parseInt(window.innerWidth-100))&&(dx >1))) { return }  
     im1.height = im1.height*size;
+    im1.width = im1.width*size;
     //im1.style.height = parseInt(im1.style.height)*size+"px";
       
       
@@ -162,8 +179,14 @@
     
   var customisations = {!! json_encode($customisations->toArray()) !!};
     document.getElementById("name").innerHTML = customisations[counter].name;
-   //alert(customisations[1].name);
-   tixt.innerHTML = customisations.length;
+
+  for(i=0; i<customisations.length; i++) {
+    createImg(i);   
+  }
+  im1 = cm[0];
+   
+   
    </script>
+
 
 @endsection
