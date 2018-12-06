8b311257efa8679054b41ef6a762623a190a3563
walter086 <wro1986@live.nl>
2018-12-06 10:13:09 +0000

    Deleted some router (payment related)

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,9 @@ Route::resource('category', 'CategoriesController');
 Route::resource('customer', 'CustomersController');
 
 
-Route::get('/payment/{order_id}', 'PaymentsController@preparePayment');
-// Route::post('/payment', 'PaymentConroller@preparePayment');
-// Route::get('webhooks/mollie/{id}', 'PaymentController@testPayment')->name('webhooks.mollie');
-// Route::post('webhooks/mollie/{id}', 'PaymentController@testPayment')->name('webhooks.mollie.callback');
 
+
+Route::get('/payment/{order_id}', 'PaymentsController@preparePayment');
 Route::name('webhooks.mollie')->post('webhooks/mollie', 'PaymentsController@handle');
 Route::get('order/succes/', 'PaymentsController@handle')->name('order.success');
 Auth::routes();
