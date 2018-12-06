2cbc0dd38967be160a7488c6a364c5282c6317c0
martenEkhart <martenekhart@hotmail.com>
2018-12-06 14:49:13 +0100

    search

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,21 +2,35 @@
 @extends('admin.index')
 
 @section('content')
-    <div class="row" style="margin-top: 20px;">
+
+{{-- 
+<div class="col-md 6" style="margin-top: 20px;">
+        <form action="/search" method="get">
+          <div class="form-group">
+              <input type="search" name="search" class="form-control">
+              <span class="form-group-btn">
+                  <button type="submit" class="btn btn-primary">Search</button>
+              </span>
+          </div>
+        </form>
+      </div> --}}
+        
+
+
+<div class="row" style="margin-top: 20px;">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading"><h3>Latest Catagories</h3></div>
-
-                <div class="panel-body">
-                    @if(count($products))
-                        <ul class="list-group">
-                          @foreach($products as $product)
+                    <div class="panel-body">
+                        @if(count($products))
+                            <ul class="list-group">
+                             @foreach($products as $product)
                             <li class="list-group-item"><a href="/product/{{$product->id}}"><h4>{{$product->name}}</h4></a><p> in Stock: {{$product->stock}}</p>
                                  <a href="/product/{{$product->id}}/edit" class="btn btn-secondary btn-lg" style= "float:right">Edit</a>
                             {!!Form::open(['action' => ['ProductsController@destroy', $product->id], 'method' => 'POST', 'class' => 'float-right'])!!}
                             {{Form::hidden('_method','DELETE')}}
                             {{Form::submit('Delete', ['class' => 'btn btn-danger btn-lg'])}}
-                         {!!Form::close()!!}
+                            {!!Form::close()!!}
                             </li>
                             @endforeach
                         </ul>
