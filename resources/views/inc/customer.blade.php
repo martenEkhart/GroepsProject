03c62c39e1fc730b3d3836460cf5159ae79a0b04
martenEkhart <martenekhart@hotmail.com>
2018-12-07 11:46:01 +0100

    opmaak van search products en found products

--- resources/views/inc/customer.blade.php
+++ resources/views/inc/customer.blade.php
@@ -1,15 +1,27 @@
 
-
+@extends('layouts.app')
+@section('content')
 @if(count($products)) 
 @foreach($products as $product)
+<a href="/product/{{$product->id}}">
+<div class="container">
+    
 <h1>{{$product->name}}</h1>
 <img style="width:10%" src="/product_images/{{$product->image_name}}">
+</a>  
 <br><br>
 <div>
     {{-- zorgt er voor dat je de html kan zien --}}
-    {!!$product->description!!} 
+  <b> Price: $</b>{!!$product->price!!}-,<br>
+  <b>Product description:</b>  {!!$product->description!!} 
 </div>  
+<br>
+<small><b>Products in stock:</b>  {{$product->stock}} <br><b>Product Added:</b>  {{$product->created_at}} </small> 
+<a href="/" class="btn btn-primary" style="margin-left:90px;">Add to Card</a>
 <hr>
-<small>Written on {{$product->created_at}} in {{$product->stock}}</small> 
+
+    
+</div>
 @endforeach
 @endif
+@endsection
