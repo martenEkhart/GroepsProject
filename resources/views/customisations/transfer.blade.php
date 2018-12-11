bc2a38378af869276c1ee7b34e777d2dd2cb61e8
Raoul Witsenburg <raoulwitsenburg@gmail.com>
2018-12-11 16:23:02 +0100

    grid buttons inplaats van de vorige buttons

--- resources/views/customisations/transfer.blade.php
+++ resources/views/customisations/transfer.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.custom')
 @section('content')
-{{-- <canvas id="canvas" style="width: 300px; height 150px; position:fixed" ></canvas> --}}
 <div class="flex-container" style="display:flex; justify-content: space-between;">
         
     <label id="name" style="font-size: 30px; color:blue" for="btnTurnL"></label>
@@ -11,72 +10,26 @@
 
 </div>
 
-<div id="dott" style="left: 0px; top: 0px; width: 25em; height:17em; border-style:dotted; border-width:2px">
+<div onscroll ="testest()" id="dott" style="left: 0px; top: 0px; width: 20em; height:14em; border-style:dotted; border-width:2px">
         .... website borders
         
 </div>  
-  
 <br />
-<div style="max-width: 400px">
-    <div id="imga"  style=" max-width:100%; width: 200px; height: 100px; position:fixed; left:0px; top: 0px">
-    </div>
-    <div class="progress" style="height: 40px;">    
-            <div id="progressOpacity" onmousemove="fnMouseOpacity(event)" class="progress-bar bg-info" role="progressbar" style="width: 100%" 
-            aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">more transparent - - - - - - - - - - - - - - -  less transparent 
-            </div>
-        </div>
-        <br />
-        <div  class="progress" style="height: 40px;">    
-        <div id="progressZindex"  onmousemove="fnMouseZindex(event)" class="progress-bar bg-success" role="progressbar" style="width: 100%" 
-        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">to the back - - - - - - - - - - - - - - -  to the front 
-        </div>
-    </div>
-</div>
-<br />
-<div>
-    <div id="horVertBtns" class="btn-group btn-group-lg" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-secondary" onclick="fnHor(-45)">LEFT</button>
-        <button type="button" class="btn btn-secondary" onclick="fnVert(-45)">TOP</button>
-        <button type="button" class="btn btn-secondary" onclick="fnVert(15)">BOTTOM</button>
-        <button type="button" class="btn btn-secondary" onclick="fnHor(15)">RIGHT</button>
-        <button type="button" class="btn btn-secondary"></button>
-        <button type="button" class="btn btn-secondary" onclick="fnHor(-1)">left</button>
-        <button type="button" class="btn btn-secondary" onclick="fnVert(-1)">top</button>
-        <button type="button" class="btn btn-secondary" onclick="fnVert(1)">bottom</button>
-        <button type="button" class="btn btn-secondary" onclick="fnHor(1)">right</button>
-    </div>
+<div class="wrapper4">
+		<div id="divUp" class="box aUp" onclick="fnVert(-1)">Up</div>
+		<div id="divDown" class="box aDown" onclick="fnVert(1)">Down</div>
+		<div id="divUpF" class="box aUpF" onclick="fnUp()">UP</div>
+		<div id="divDownF" class="box aDownF" onclick="fnDown()">DOWN</div>
+		<div id="divLeft" class="box aLeft" onclick="fnLeft()">Left</div>
+        <div id="divRight" class="box aRight" onclick="fnRight()">Right</div>
+        <div id="divSmaller" class="boxSmallBig aSmaller" onclick="fnSmaller(5)">Smaller</div>
+        <div id="divBigger" class="boxSmallBig aBigger">Bigger</div>
+        <div id="divDone" class="box aDone">Done</div>
+        <div id="divTransparency" class="boxtransparency aTransparancy" onmousemove="fnMouseOpacity()">transparency</div>
+        <div id="divZindex" class="box aZindex" onmousemove="fnMouseZindex()">back - front</div>
 </div>
 
-<div id="smallBigBtns">
-        <br />
-    <div class="btn-group btn-group-lg" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-secondary" onclick="fnSize(1/1.1)">SMALLER</button>
-        <button type="button" class="btn btn-secondary"  onclick="fnSize(1.1)">BIGGER</button>
-        <button type="button" class="btn btn-secondary"></button>
-        <button type="button" class="btn btn-secondary"></button>
-        <button type="button" class="btn btn-secondary" ></button>
-        <button type="button" class="btn btn-secondary" onclick="fnDone()">Done</button>
-        <button type="button" class="btn btn-secondary"></button>
-        <button type="button" class="btn btn-secondary"></button>
-        <button type="button" class="btn btn-secondary"></button>
-        <button type="button" class="btn btn-secondary" onclick="fnSize(1/1.0005)">smaller</button>
-        <button type="button" class="btn btn-secondary" onclick="fnSize(1.01)">bigger</button>
-    </div>
-</div>
 
-<div id="list" style="position:absolute">
-   
-</div>
-      
-{{-- <div class="flex-container" style="display:flex; justify-content: space-between;">
-        
-        <label id="name2" style="font-size: 30px; color:blue" for="btnTurnL2"></label>
-            <button id="btnTurnL2" onclick="fnTurn(-1)" style="width: 100px; height: 40px; font-size:20px;" > < </button>
-            <button onclick="fnTurn(1)" style="width: 100px; height: 40px; font-size:20px;"> > </button>
-            <input style="height: 50px" id="checkbox2" type="checkbox" name="vehicle3" value="1" checked>Visible<br>
-                
-    
-</div> --}}
 @csrf
 <button onclick="loadDoc('POST', '/customisations/changedata', changeDiv, 'tixt',  'name')">send</button>
     
@@ -84,34 +37,16 @@
 
 </div>
 <script>
-
-function createImg(nr) {
-    c = customisations[nr];
-    cm[nr] = document.createElement('img');
-    cm[nr].style.position = "absolute";
-    cm[nr].src = "../images/customisations/" + c.image_name;
-    cm[nr].style.left = c.x+ dotted.left + "px";
-    cm[nr].style.top = c.y + dotted.top + "px";
-    cm[nr].height = c.height;
-    cm[nr].width = c.width;
-    cm[nr].style.zIndex = c.z_layer;
-    cm[nr].style.opacity = c.opacity/100;
-    document.body.appendChild(cm[nr]);
-    
-}   
+function testest(){
+    alert("testst");
+}
 
  var counter = 0;
  var cm = [];
 
  var dotted = document.getElementById("dott").getBoundingClientRect();
-//  var img = document.getElementById("img");
-//  img.style.opacity = 0.7;
-//  img.style.left = dotted.left +"px";
-//  img.style.top = dotted.top +"px";
- 
 
   var tel = 0;
-//   im1 = document.getElementById("img");
   tixt = document.getElementById("tixt");
 
   var customisations = {!! json_encode($customisations->toArray()) !!};
@@ -122,19 +57,33 @@ function createImg(nr) {
   }
   
   im1 = cm[0];
-   
+
+  function createImg(nr) {
+    c = customisations[nr];
+    cm[nr] = document.createElement('img');
+    cm[nr].style.position = "absolute";
+    cm[nr].src = "../images/customisations/" + c.image_name;
+    cm[nr].style.left = c.x+  "px";
+    cm[nr].style.top = c.y +  "px";
+    cm[nr].height = c.height;
+    cm[nr].width = c.width;
+    cm[nr].style.zIndex = c.z_layer;
+    cm[nr].style.opacity = c.opacity/100;
+    document.body.appendChild(cm[nr]);    
+}   
+
     function updateForm() {
 
     }
   
-    function fnTurn(dx) {
-        if((counter <= 0)&&(dx == -1)) {
+    function fnTurn(nr) {
+        if((counter <= 0)&&(nr == -1)) {
             counter = customisations.length-1;
         } else {
-            if ((counter >= customisations.length-1)&&(dx == 1)) {
+            if ((counter >= customisations.length-1)&&(nr == 1)) {
                 counter = 0;
             } else {
-                counter += dx;
+                counter += nr;
             }
 
         }
@@ -143,46 +92,61 @@ function createImg(nr) {
 
     function fnVolgende(counter) {
         document.getElementById("name").innerHTML = customisations[counter].name+ " id:"+customisations[counter].id;
-     //   document.getElementById("name2").innerHTML = customisations[counter].name+ " id:"+customisations[counter].id;
         im1 = cm[counter];
-        // im1.src = "../images/customisations/" + customisations[counter].image_name;
-        // im1.style.left = customisations[counter].x;
-        // im1.style.top = customisations[counter].y;
-        // im1.height = customisations[counter].height;
-        // im1.width = customisations[counter].width;
-        // im1.style.zIndex = customisations[counter].z_layer;
-        // im1.style.opacity = customisations[counter].opacity/100;
         document.getElementById("checkbox").checked = customisations[counter].visible;
-     //   document.getElementById("checkbox2").checked = customisations[counter].visible;
     }
 
-    function fnMouseZindex(e) {
-        im1.style.zIndex = e.clientX-parseInt(document.getElementById("progressZindex").style.width);
-       tixt.innerHTML = e.clientX-(parseInt(document.getElementById("progressZindex").style.width));
+    function fnMouseZindex() {
+        im1.style.zIndex = Math.floor((event.offsetX/document.getElementById("divZindex").clientWidth - 0.5)*100 );
+       tixt.innerHTML = Math.floor((event.offsetX/document.getElementById("divZindex").clientWidth - 0.5)*100 );
         // alert(e.clientX);
     }
 
-    function fnMouseOpacity(e) {
-        im1.style.opacity = (e.clientX-50)/(parseInt(document.getElementById("progressOpacity").style.width)*3);
+    function fnMouseOpacity() {
+        tixt.innerHTML = event.offsetX/document.getElementById("divTransparency").clientWidth;
+        im1.style.opacity = (event.offsetX/document.getElementById("divTransparency").clientWidth)*1.5-0.05;
         
        // alert(e.clientX);
     }
 
   function fnHor (dx) {
-    if(((parseInt(im1.style.left)<0)&&(dx<0))||((parseInt(im1.style.left)>parseInt(window.innerWidth-100))&&(dx >0))) { return }
+    if(((parseInt(im1.style.left)<30)&&(dx<0))||((parseInt(im1.style.left)>parseInt(window.innerWidth-150))&&(dx >0))) { return }
+    customisations[counter].x = parseInt(im1.style.left);
+    im1.style.left = customisations[counter].x+dx +"px";  
     
-    im1.style.left = parseInt(im1.style.left)+dx +"px";  
+
   }
 
-  function fnDone() {
-       document.getElementById("smallBigBtns").style.display = "none";
-      document.getElementById("horVertBtns").style.display = "none";
+  function fnLeft () {
+   let dx = event.offsetX/document.getElementById("divLeft").clientWidth;
+   if (dx < 0.5) { fnHor(-1*(76-(dx*150)))} else { fnHor(-1)}
+ //  tixt.innerHTML = document.getElementById("divLeft").clientWidth;
+  }
 
+  function fnRight () {
+   let dx = event.offsetX/document.getElementById("divRight").clientWidth;
+   if (dx > 0.5) { fnHor((76-((1-dx)*150)))} else { fnHor(1)}
+  }
+
+  function fnUp() {
+    let dy = event.offsetX/document.getElementById("divUpF").clientWidth;
+    if (dy > 0.15) {fnVert(-1*(dy*50)-1); } else {fnVert(-1)}
+    }
+  
+  function fnDown() {
+    let dy = event.offsetX/document.getElementById("divDownF").clientWidth;
+    if (dy > 0.15) {fnVert(dy*50+1);} else { fnVert(1)}
+        
+  }
+
+    function fnDone() {
+    loadDoc('POST', '/customisations/changedata', changeDiv, 'tixt',  'name')
   }
  
-  function fnVert (dx) {
-    if(((parseInt(im1.style.top)<-70)&&(dx<0))||(((parseInt(im1.style.top)+parseInt(im1.height))>window.innerHeight-50)&&(dx >0))) { return }
-    im1.style.top = parseInt(im1.style.top)+dx +"px";  
+  function fnVert (dy) {
+    if(((parseInt(im1.style.top)<-20)&&(dy<0))||(((parseInt(im1.style.top)+parseInt(im1.height))>window.innerHeight-150)&&(dy >0))) { return }
+    customisations[counter].y = parseInt(im1.style.top);
+    im1.style.top = customisations[counter].y+dy +"px"; 
    }
  
   function fnSize(size) {
@@ -191,6 +155,11 @@ function createImg(nr) {
     im1.width = im1.width*size;
     //im1.style.height = parseInt(im1.style.height)*size+"px";     
     }
+
+    function fnSmaller(e) {
+      tixt.innerHTML = e+"tetjjjk";
+  }
+  
   
     function loadDoc(method, url, myFunction, div, input) {
     if (window.XMLHttpRequest) {
@@ -206,9 +175,6 @@ function createImg(nr) {
     xhttp.open(method, url, true);
     if (method == 'POST') {
         alert(objToString(customisations[counter]));
-     //   alert(input);
-     //  var data = input + "=" + document.getElementById(input).value;
-        //  var data = "name=iets&waf=nog&beer=kroelie" //JSON.stringify(customisations[0]); //input + "=" + document.getElementById(input).value;
         var data = objToString(customisations[counter]);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.setRequestHeader("X-CSRF-TOKEN", document.querySelector("meta[name='csrf-token']").getAttribute("content"));
@@ -237,8 +203,7 @@ function objToString (obj) {
 function changeDiv(xhttp, div) {
     document.getElementById(div).innerHTML = xhttp.responseText;
 }
-  
-   
+     
    </script>
 
 
--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -3,21 +3,106 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet"  href="/css/app.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> --}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>JAAW</title>
+    <style>
+        .wrapper4 {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            grid-template-rows: repeat(4, 1fr);
+            grid-gap: 0.4em;
+            background-color: #fff;
+            color: #444;
+        }
 
+        div {
+            text-align: center
+        }
+        .box {
+		background-color: rgb(120,70,123);
+		border: 5px solid rgb(88,55,112);
+		color: #fff;
+		border-radius: 5px;
+		padding: 0px;
+		font: 150%/1.3 Lucida Grande,Lucida Sans Unicode,Lucida Sans,Geneva,Verdana,sans-serif;
+
+	}
+	.boxtransparency {
+		background-image: linear-gradient(to right, rgb(219, 204, 223), rgb(121, 41, 212));
+		/* background-color: rgb(60, 129, 161); */
+		border: 5px solid rgb(88,55,112);
+		color: #fff;
+		border-radius: 5px;
+		padding: 0px;
+		font: 150%/1.3 Lucida Grande,Lucida Sans Unicode,Lucida Sans,Geneva,Verdana,sans-serif;
+
+	}
+
+	.boxSmallBig {
+		background-color: rgb(60, 129, 161);
+		border: 5px solid rgb(88,55,112);
+		color: #fff;
+		border-radius: 5px;
+		padding: 0px;
+		font: 150%/1.3 Lucida Grande,Lucida Sans Unicode,Lucida Sans,Geneva,Verdana,sans-serif;
+
+	}
+
+	.aUp { 
+		grid-column: 2 / 3; 
+		grid-row: 1 / 2;
+	}
+	.aDown { 
+		grid-column: 2 /3; 
+		grid-row: 3 / 4;
+	}
+	.aLeft { 
+		grid-column: 1 / 2; 
+		grid-row: 2 /3;
+	}
+	.aRight { 
+		grid-column: 3 / 4; 
+		grid-row: 2 / 3;
+	}
+	.aDone { 
+		grid-column: 2 / 3; 
+		grid-row: 2 /3  ;
+	}
+	.aTransparancy { 
+		grid-column: 1 / 3; 
+		grid-row: 4 / 5 ;
+	}
+	.aZindex { 
+		grid-column: 3 / 5; 
+		grid-row: 4 / 5 ;
+	}
+	.aUpF { 
+		grid-column:4 / 5; 
+		grid-row: 1 / 2 ;
+	}
+	.aDownF { 
+		grid-column: 4 / 5; 
+		grid-row: 3 / 4 ;
+	}
+	.aSmaller { 
+		grid-column: 3 /4; 
+		grid-row: 3 / 4 ;
+	}
+	.aBigger { 
+		grid-column: 3 / 4; 
+		grid-row: 1 / 2 ;
+	}
+
+    </style>
 </head>
 <body>
     @include('inc.navbar')
 
    
     <div class="container" style="margin-top: 10px;" >
-         @if(Request::is('/'))   
-         @include('inc.showcase')
-         @endif
          
          <div class="row">
           
@@ -28,7 +113,7 @@
         </div> 
             </div> 
 
-            <footer id="footer" class="text-center" style= "margin-top: 45%;" >
+            <footer id="footer" class="text-center" style= "margin-top: 1%;" >
                 <p><b>Copyright 2018 &copy; JAAW</b></p>
             </footer>          
     <script src="{{ asset('js/app.js') }}" defer></script>
