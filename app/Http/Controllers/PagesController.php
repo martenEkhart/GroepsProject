5287a476b10729c8eea8bde9b8ca33e8ff838538
martenEkhart <martenekhart@hotmail.com>
2018-12-13 14:59:14 +0100

    bull

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,30 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 use Auth;
 use App\Product;
 
 
 
-=======
 
-use Auth;
-use App\Product;
-<<<<<<< HEAD
- 
-=======
-=======
-use Auth; 
-use App\Product; 
-
-<<<<<<< HEAD
-=======
->>>>>>> 4725180a904c1bcef5072a9cc45d9601c1cb7f85
->>>>>>> 98575147b3abaf9a38b2ec4ac1d3bfbb04846406
->>>>>>> ea6b4e749f7c6f4f5faa597219117a064dd06cfb
->>>>>>> 718c1f98b350631eb00c25b6fe77e7ec393e30b8
->>>>>>> 5c376b42025bd23ba81058cee1a61f7a3c5b100e
 
 class PagesController extends Controller
 {
