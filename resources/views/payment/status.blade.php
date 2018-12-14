8bc30d416a0b487ca5aa4b06c2550ea95cfe7abf
walter <wro1986@live.nl>
2018-12-14 15:52:21 +0100

    Fix status payment

--- resources/views/payment/status.blade.php
+++ resources/views/payment/status.blade.php
@@ -3,14 +3,16 @@
 
 
                         <ul class="list-group">
+                        @php var_dump ($payments) @endphp
                          @foreach($payments as $payment)
                          <div class="for-wrapper">
                         Order id:
                         {{$payment->order_id}}
                         Payment status:
                         {{$payment->status}}
-
+                        
                         </li>
                     </div>
                         @endforeach
+                </ul>
 @endsection
