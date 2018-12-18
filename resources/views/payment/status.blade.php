cd92aec31c933fabce9dc74c6855b08cded3ba21
walter086 <wro1986@live.nl>
2018-12-18 10:13:31 +0000

    CsS additions

--- resources/views/payment/status.blade.php
+++ resources/views/payment/status.blade.php
@@ -11,17 +11,17 @@
                         <b>Payment status:</b><br>
                          @switch($payment->status)
                             @case(0)
-                                Payment open
+                            <p class="feedback_warning">Payment open</p>
                             @break
                             @case(1)
-                                Payment cancelled
+                            <p class="feedback_warning">Payment cancelled</p>
                              @break
                             @case(2)
-                               <p class="feedback_okay">Payment received!</p>
-                                @break
-                                @case(5)
-                                Payment Expired!
-                                @break
+                            <p class="feedback_okay">Payment received!</p>
+                            @break
+                            @case(5)
+                            <p class="feedback_warning">Payment Expired!</p>
+                            @break
                             
                         @endswitch
                      
