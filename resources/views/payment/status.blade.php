6a26b9be83e80cd140d94cdd7f82e98e6f182f6f
walter086 <wro1986@live.nl>
2018-12-18 12:43:54 +0000

    Payment controller  updated

--- resources/views/payment/status.blade.php
+++ resources/views/payment/status.blade.php
@@ -14,6 +14,9 @@
                              @break
                             @case(2)
                             <p class="feedback_okay">Payment received!</p>
+                            Thanks for shopping at Just An Affordable Webshop!<br>
+                            Your payment is received and we'll start packing your order.<br>
+                            You'll get an email when the order is shipped!<br>
                             @break
                             @case(5)
                             <p class="feedback_warning">Payment Expired!</p>
