405a0137188931c29e99d41bbd86fb53d66864ac
walter086 <wro1986@live.nl>
2018-12-18 12:07:42 +0000

    Payment controller  updated

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -166,8 +166,8 @@ public function result(Request $request){
         $user_id = Auth::user()->id;
     }
     // $payments = Payment::where('user_id',$user_id)->first();
-    $payments = Payment::where('user_id',$user_id)->latest()->first();
-    return view('payment.status')->with('payments', $payments);
+    $payments = Payment::where('user_id',$user_id)->first();
+    return view('payment.status')->with('payments', $payments->reverse());
 }  
 
 
